102d38c7c930cc11e370177dd3ed4ff171b2d5e3
LevanceWam <lkwamley@fullsail.edu>
2020-10-13 21:16:18 -0400

    Created a queue using a array

---
+++ Queues/ExerciseBuildingAQueueUsingArrays/ArrayQueue.js
@@ -0,0 +1,31 @@
+const _elements = new WeakMap();
+
+class ArrayQueue{
+    constructor(){
+        _elements.set(this, []);
+    }
+
+    enqueue(item){
+       return _elements.get(this).push(item)
+    }
+
+    dequeue(){
+        if (_elements.get(this).length==0) throw new Error('Queue is empty');
+        return _elements.get(this).shift();
+    }
+
+    peek(){
+        if (_elements.get(this).length==0) throw new Error('Queue is empty');
+        return _elements.get(this)[0];
+    }
+
+    isEmpty(){
+        return (_elements.get(this).length==0);
+    }
+
+    getQueue(){
+        return _elements.get(this);
+    }
+}
+
+module.exports = ArrayQueue;
